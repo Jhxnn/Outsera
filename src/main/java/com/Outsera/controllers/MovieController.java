18eb5c4b64753d1a6cd8b516c946624a32c75cd1
Hilixx <jhonalizan@gmail.com>
2025-04-17 20:00:54 -0300

    documentario

--- src/main/java/com/Outsera/controllers/MovieController.java
+++ src/main/java/com/Outsera/controllers/MovieController.java
@@ -3,6 +3,7 @@ package com.Outsera.controllers;
 import com.Outsera.dtos.MovieDto;
 import com.Outsera.models.Movie;
 import com.Outsera.services.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +19,30 @@ public class MovieController {
     @Autowired
     MovieService movieService;
 
+    @Operation(description = "Lista todos os filmes")
     @GetMapping
     public ResponseEntity<List<Movie>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(movieService.findAll());
     }
+
+    @Operation(description = "Busca filme por id")
     @GetMapping("/{id}")
     public ResponseEntity<Movie> findById(@PathVariable(name = "id")UUID id){
         return ResponseEntity.status(HttpStatus.OK).body(movieService.findById(id));
     }
+
+    @Operation(description = "Cria um filme")
     @PostMapping
     public ResponseEntity<Movie> createMovie(@RequestBody MovieDto movieDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(movieService.createMovie(movieDto));
     }
+    @Operation(description = "Atualiza um filme")
     @PutMapping("/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable(name = "id")UUID id,
                                              @RequestBody MovieDto movieDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(movieService.updateMovie(id,movieDto));
     }
+    @Operation(description = "Deleta um filme")
     @DeleteMapping("/{id}")
     public ResponseEntity<Movie> deleteMovie(@PathVariable(name = "id")UUID id){
         movieService.deleteMovie(id);
